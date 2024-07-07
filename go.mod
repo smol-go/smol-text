33f9bac4685b90c2b02724d518047fe201b6f5e9
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-07-07 10:48:03 +0530

    termbox running and added ASCII art

---
+++ go.mod
@@ -0,0 +1,7 @@
+module smoltext
+
+go 1.22.5
+
+require github.com/nsf/termbox-go v1.1.1
+
+require github.com/mattn/go-runewidth v0.0.9 // indirect
---
+++ src/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mattn/go-runewidth"
+	"github.com/nsf/termbox-go"
+)
+
+func print_message(x, y int, foreground, background termbox.Attribute, msg string) {
+	for _, c := range msg {
+		termbox.SetCell(x, y, c, foreground, background)
+		x += runewidth.RuneWidth(c)
+	}
+}
+
+func run_editor() {
+	err := termbox.Init()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	asciiArt := `
+		███████╗███╗   ███╗ ██████╗ ██╗  ████████╗███████╗██╗  ██╗████████╗
+		██╔════╝████╗ ████║██╔═══██╗██║  ╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝
+		███████╗██╔████╔██║██║   ██║██║     ██║   █████╗   ╚███╔╝    ██║   
+		╚════██║██║╚██╔╝██║██║   ██║██║     ██║   ██╔══╝   ██╔██╗    ██║   
+		███████║██║ ╚═╝ ██║╚██████╔╝███████╗██║   ███████╗██╔╝ ██╗   ██║   
+		╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝ 
+	`
+
+	lines := strings.Split(asciiArt, "\n")
+
+	maxWidth := 0
+	for _, line := range lines {
+		width := runewidth.StringWidth(line)
+		if width > maxWidth {
+			maxWidth = width
+		}
+	}
+
+	message := "A simple text editor written in golang"
+
+	// calculating the starting x position for message
+	startX := (maxWidth - runewidth.StringWidth(message)) / 2
+
+	for i, line := range lines {
+		print_message(0, i, termbox.ColorCyan, termbox.ColorDefault, line)
+	}
+
+	print_message(startX, len(lines), termbox.ColorCyan, termbox.ColorDefault, message)
+
+	termbox.Flush()
+	termbox.PollEvent()
+	termbox.Close()
+}
+
+func main() {
+	run_editor()
+}
